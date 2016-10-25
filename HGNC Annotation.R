b71abea2aba0389fd513f9369c0e84ffe04abb5e
Peter Chernek <peter.chernek@gmail.com>
2016-10-24 22:40:41 -0400

    Refseq from mirdb to HGNC Symbols.

---
+++ HGNC Annotation.R
@@ -0,0 +1,21 @@
+######  Converting Refseq identifiers to HGNC symbols with biomaRt ###########
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("biomaRt")
+library(biomaRt)
+library(readr)
+
+
+x <- read_csv("https://raw.githubusercontent.com/pchernek/tcga_prad/master/miRDB_v5.0_prediction_result.csv")
+
+x<-miRDB_v5.0_prediction_result
+
+x<-as.vector(x)
+
+mart = useMart("ensembl", dataset="hsapiens_gene_ensembl")
+
+listFilters(mart)
+
+ann <- getBM(c("hgnc_symbol"),"refseq_mrna_predicted", x[1], mart)
+
+ann1 <- getBM(c("hgnc_symbol"),"refseq_mrna", x[1], mart)
