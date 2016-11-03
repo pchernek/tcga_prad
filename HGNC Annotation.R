687c0a2f2ad917519e6c13dc9f7665257a8764e4
Peter Chernek <peter.chernek@gmail.com>
2016-11-02 22:44:07 -0400

    HGNC to GMT
    WriteGMT using LeviRmisc

--- HGNC Annotation.R
+++ HGNC Annotation.R
@@ -4,7 +4,7 @@ source("https://bioconductor.org/biocLite.R")
 biocLite("biomaRt")
 library(biomaRt)
 library(readr)
-
+library(LeviRmisc)
 
 x <- read_csv("https://raw.githubusercontent.com/pchernek/tcga_prad/master/miRDB_v5.0_prediction_result.csv")
 
@@ -18,4 +18,22 @@ listFilters(mart)
 
 ann <- getBM(c("hgnc_symbol"),"refseq_mrna_predicted", x[1], mart)
 
+
 ann1 <- getBM(c("hgnc_symbol"),"refseq_mrna", x[1], mart)
+
+x<-as.list(ann1)
+
+y<-as.list(ann)
+
+z<-c(x, y)
+
+
+LeviRmisc::writeGMT(z, fname = "HGNC Symbols")
+
+
+
+
+
+
+
+
